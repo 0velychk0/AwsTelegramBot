e50ce5dfcb3a93a20e328477da7e5337d480b5a6
Oleksandr_Velychko2 <Alex.Velichko@gmail.com>
2022-04-13 15:34:31 +0300

    Call Lambda to save TeleUser data

--- src/main/java/com/ovelychko/SimpleTelegramWebhookBot.java
+++ src/main/java/com/ovelychko/SimpleTelegramWebhookBot.java
@@ -1,7 +1,9 @@
 package com.ovelychko;
 
+import java.util.StringJoiner;
+
 import com.fasterxml.jackson.databind.ObjectMapper;
-import lombok.extern.slf4j.Slf4j;
+
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.utils.URIBuilder;
 import org.apache.http.impl.client.CloseableHttpClient;
@@ -13,20 +15,57 @@ import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.methods.send.SendPhoto;
 import org.telegram.telegrambots.meta.api.objects.InputFile;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.User;
 
-import java.util.StringJoiner;
+import lombok.extern.slf4j.Slf4j;
+import software.amazon.awssdk.core.SdkBytes;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.lambda.LambdaClient;
+import software.amazon.awssdk.services.lambda.model.InvokeRequest;
+import software.amazon.awssdk.services.lambda.model.InvokeResponse;
+import software.amazon.awssdk.services.lambda.model.LambdaException;
 
 @Slf4j
 public class SimpleTelegramWebhookBot extends TelegramWebhookBot {
 
-    private final String webHookPath = "dd";
-    private final String userName = "dd";
-    private final String botToken = "dd";
-    private final String omdbapiKey = "dd";
-    private final String imbdLink = "dd";
+    private final ObjectMapper mapper = new ObjectMapper();
+
+    private final String webHookPath = "https://6fu5ajzxsi.execute-api.eu-west-3.amazonaws.com/default/AwsTelegramBotLambda";
+    private final String userName = "@movieCatalogUserBot";
+    private final String botToken = "5277197008:AAGBu7sT0G59MCi0r_fGw_6JucDw4ieZsFo";
+    private final String omdbapiKey = "1ac1214b";
+    private final String imbdLink = "https://www.imdb.com/title/";
 
     public SimpleTelegramWebhookBot() {
         log.info("WebhookTelegramController created");
+    }
+
+    public void invokeFunction(TelegramUserData telegramUserData) {
+        log.info("invokeFunction started");
+
+        String functionName = "AwsTelegramUserDataAdd";
+        LambdaClient awsLambda = LambdaClient.builder()
+                .region(Region.EU_WEST_3)
+                .build();
+
+        InvokeResponse res = null;
+        try {
+            String json = mapper.writeValueAsString(telegramUserData);
+            SdkBytes payload = SdkBytes.fromUtf8String(json);
+
+            InvokeRequest request = InvokeRequest.builder()
+                    .functionName(functionName)
+                    .payload(payload)
+                    .build();
+
+            res = awsLambda.invoke(request);
+            String value = res.payload().asUtf8String();
+            log.info(value);
+        } catch (Exception e) {
+            log.error(e.getMessage());
+        }
+        awsLambda.close();
+        log.info("invokeFunction finished");
     }
 
     @Override
@@ -36,7 +75,18 @@ public class SimpleTelegramWebhookBot extends TelegramWebhookBot {
         String searchText = update.getMessage().getText().trim();
         log.info("Search Text:{}", searchText);
 
-        ObjectMapper mapper = new ObjectMapper();
+        User user = update.getMessage().getFrom();
+        TelegramUserData telegramUserData = new TelegramUserData(
+                user.getId(),
+                user.getFirstName(),
+                user.getIsBot(),
+                user.getLastName(),
+                user.getUserName(),
+                user.getLanguageCode(),
+                user.getCanJoinGroups(),
+                user.getCanReadAllGroupMessages(),
+                user.getSupportInlineQueries());
+        invokeFunction(telegramUserData);
 
         try (CloseableHttpClient client = HttpClients.createDefault()) {
             URIBuilder builder = new URIBuilder();
