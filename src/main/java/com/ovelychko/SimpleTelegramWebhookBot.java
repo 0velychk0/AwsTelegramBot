93edc69a311c6ccc72e731dcec0a39c05d18065b
Oleksandr_Velychko2 <Alex.Velichko@gmail.com>
2022-04-13 12:11:29 +0300

    Fix bug in handling movie without PhotoUrl

--- src/main/java/com/ovelychko/SimpleTelegramWebhookBot.java
+++ src/main/java/com/ovelychko/SimpleTelegramWebhookBot.java
@@ -1,19 +1,20 @@
 package com.ovelychko;
 
-import java.util.StringJoiner;
-
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.utils.URIBuilder;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.TextUtils;
 import org.telegram.telegrambots.bots.TelegramWebhookBot;
 import org.telegram.telegrambots.meta.api.methods.BotApiMethod;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.methods.send.SendPhoto;
 import org.telegram.telegrambots.meta.api.objects.InputFile;
 import org.telegram.telegrambots.meta.api.objects.Update;
+
+import java.util.StringJoiner;
 
 @Slf4j
 public class SimpleTelegramWebhookBot extends TelegramWebhookBot {
@@ -63,12 +64,20 @@ public class SimpleTelegramWebhookBot extends TelegramWebhookBot {
                 this.execute(sendMessage);
 
                 for (MovieSearchModel model : response.search) {
-                    SendPhoto sendPhoto = new SendPhoto();
-                    sendPhoto.setChatId(update.getMessage().getChatId().toString());
-                    sendPhoto.setCaption(String.format("%s - %s (%s), https://www.imdb.com/title/%s \n",
-                            model.getType(), model.getTitle(), model.getYear(), model.getImdbID(), model.getImdbID()));
-                    sendPhoto.setPhoto(new InputFile(model.getPoster()));
-                    this.execute(sendPhoto);
+                    if (!TextUtils.isBlank(model.getPoster()) && model.getPoster().startsWith("http")) {
+                        SendPhoto sendPhoto = new SendPhoto();
+                        sendPhoto.setChatId(update.getMessage().getChatId().toString());
+                        sendPhoto.setCaption(String.format("%s - %s (%s),\n https://www.imdb.com/title/%s",
+                                model.getType(), model.getTitle(), model.getYear(), model.getImdbID()));
+                        sendPhoto.setPhoto(new InputFile(model.getPoster()));
+                        this.execute(sendPhoto);
+                    } else {
+                        SendMessage textMessage = new SendMessage();
+                        textMessage.setChatId(update.getMessage().getChatId().toString());
+                        textMessage.setText(String.format("%s - %s (%s),\n https://www.imdb.com/title/%s",
+                                model.getType(), model.getTitle(), model.getYear(), model.getImdbID()));
+                        this.execute(textMessage);
+                    }
                 }
             } else {
                 SendMessage sendMessage = new SendMessage();
