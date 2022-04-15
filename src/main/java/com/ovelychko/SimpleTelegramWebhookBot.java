0e07649a1704029598abe441c9667032369de24b
Oleksandr_Velychko2 <Alex.Velichko@gmail.com>
2022-04-15 15:36:19 +0300

    Update

--- src/main/java/com/ovelychko/SimpleTelegramWebhookBot.java
+++ src/main/java/com/ovelychko/SimpleTelegramWebhookBot.java
@@ -67,7 +67,6 @@ public class SimpleTelegramWebhookBot extends TelegramWebhookBot {
 
                         StringJoiner caption = new StringJoiner("\n");
                         caption.add(String.format("%s - %s (%s),", model.getType(), model.getTitle(), model.getYear()));
-                        caption.add(String.format("More details: t.me/movieCatalogUserBot?start=getDetail-%s,", model.getImdbID()));
                         caption.add(String.format("imdb: %s,", config.getImbdLink() + model.getImdbID()));
                         sendPhoto.setCaption(caption.toString());
 
